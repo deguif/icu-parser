23ab9486f26818c29d44dc6cb7fbc2472912cdf8
François-Xavier de Guillebon <deguif@gmail.com>
2022-06-07 12:58:35 +0200

    Fix configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,7 +17,7 @@ return ($config = new PhpCsFixer\Config())
         'list_syntax' => ['syntax' => 'short'],
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         'native_constant_invocation' => true,
-        'native_function_invocation' => true,
+        'native_function_invocation' => ['include' => ['@all']],
         'no_alias_functions' => true,
         'no_useless_else' => true,
         'nullable_type_declaration_for_default_null_value' => true,
@@ -28,7 +28,7 @@ return ($config = new PhpCsFixer\Config())
         'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
         'static_lambda' => true,
         'ternary_to_null_coalescing' => true,
-        'visibility_required' => ['property', 'method', 'const'],
+        'visibility_required' => ['elements' => ['property', 'method', 'const']],
         'void_return' => true,
     ])
 ;
